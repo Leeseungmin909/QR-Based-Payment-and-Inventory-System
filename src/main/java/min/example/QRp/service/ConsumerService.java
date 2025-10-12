48387f384a39dfd9ef14bad14ed43407a6b7cd18
leeseungmin <cldkdi1@naver.com>
2025-10-12 19:44:17 +0900

    findAllProducts,findPurchaseById 삭제

--- src/main/java/min/example/QRp/service/ConsumerService.java
+++ src/main/java/min/example/QRp/service/ConsumerService.java
@@ -11,10 +11,8 @@ import min.example.QRp.repository.ProductRepository;
 import min.example.QRp.repository.PurchaseRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -33,17 +31,6 @@ public class ConsumerService {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new EntityNotFoundException("해당 ID의 상품을 찾을 수 없습니다." + productId));
         return new ProductResponseDto(product);
-    }
-
-    /**
-     * 모든 제품 검색
-     * @return 모든 제품
-     */
-    public List<ProductResponseDto> findAllProducts(){
-        List<Product> productList = productRepository.findAll();
-        return productList.stream()
-                .map(product -> new ProductResponseDto(product))
-                .collect(Collectors.toList());
     }
 
     /**
@@ -69,15 +56,5 @@ public class ConsumerService {
         Purchase purchase = Purchase.createPurchase(purchaseItems.toArray(new PurchaseItem[0]));
         purchaseRepository.create(purchase);
         return purchase;
-    }
-
-    /**
-     * 영수증 조회 메소드
-     * @param purchaseId 조회할 구매 id
-     * @return 영수증 내역
-     */
-    public Purchase findPurchaseById(int purchaseId){
-        return purchaseRepository.findById(purchaseId)
-                .orElseThrow(() -> new EntityNotFoundException("해당 ID의 구매 내역을 찾을 수 없습니다." + purchaseId));
     }
 }
