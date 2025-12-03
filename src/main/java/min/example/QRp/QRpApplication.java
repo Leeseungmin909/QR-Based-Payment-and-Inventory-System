a7dc69505639e729d66c66fb94e4c830426232d0
leeseungmin <cldkdi1@naver.com>
2025-12-04 00:57:07 +0900

    chore: RestTemplate Bean 추가

--- src/main/java/min/example/QRp/QRpApplication.java
+++ src/main/java/min/example/QRp/QRpApplication.java
@@ -2,6 +2,8 @@ package min.example.QRp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class QRpApplication {
@@ -10,4 +12,8 @@ public class QRpApplication {
 		SpringApplication.run(QRpApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
